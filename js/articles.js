cfd2ce4338a605961a83306ce03d35ae4009d231
Tomás Iannello <106415807+iannellotomas@users.noreply.github.com>
2023-02-27 08:31:11 -0300

    Delete js directory

--- js/articles.js
+++
@@ -1,9 +0,0 @@
-$('.section__open').click(function(){
-    let parentSection = $(this, '.section__open').parent().parent();
-    if (parentSection.hasClass('expand')){
-        parentSection.removeClass('expand');
-    } else {
-        $('.section').removeClass('expand');
-        parentSection.addClass('expand');
-    }
-});
